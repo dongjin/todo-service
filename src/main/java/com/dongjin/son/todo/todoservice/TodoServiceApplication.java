cb4fc9a1ee0d1bce8833ff2e545aff10919c3e28
dson <dongjin.son@hbo.com>
2021-01-07 17:50:55 -0800

    to create war file (instead of jar)

--- src/main/java/com/dongjin/son/todo/todoservice/TodoServiceApplication.java
+++ src/main/java/com/dongjin/son/todo/todoservice/TodoServiceApplication.java
@@ -4,12 +4,27 @@ import com.dongjin.son.todo.todoservice.services.RealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoServiceApplication {
+public class TodoServiceApplication extends SpringBootServletInitializer {
+
+	// (3) To Build as a WAR
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoServiceApplication.class, args);
 	}
 
 }
+//public class TodoServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TodoServiceApplication.class, args);
+//	}
+//
+//}
